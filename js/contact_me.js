324d9513219bd3e558371d771b78a86f723355d3
Tom Herrick <therrick@gmail.com>
2014-12-14 21:10:52 -0500

    hook up contact form using brace.io

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,8 +18,9 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//forms.brace.io/" + to_email, // to_email is set globally in js.html based on site variables
                 type: "POST",
+                dataType: "json",
                 data: {
                     name: name,
                     phone: phone,
